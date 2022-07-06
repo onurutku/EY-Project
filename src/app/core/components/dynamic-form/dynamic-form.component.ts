b79336372799491c61b7566b9ed241784a96ef36
onur.topaloglu <onur.topaloglu@orioninc.com>
2022-07-06 20:36:06 +0300

    initialize

--- src/app/core/components/dynamic-form/dynamic-form.component.ts
+++ src/app/core/components/dynamic-form/dynamic-form.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { DynamicForm } from 'src/app/shared/models/dynamic-form.model';
-
+import Inputs from 'dynamic-form.json';
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
@@ -9,17 +14,22 @@ import { DynamicForm } from 'src/app/shared/models/dynamic-form.model';
 })
 export class DynamicFormComponent implements OnInit {
   // type!: 'text' | 'select';
-  dynamicForm!: DynamicForm;
+  dynamicForm: DynamicForm[] = Object.assign(Inputs.inputs);
   myForm!: FormGroup;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm = this.fb.group({
-      dynamicField: null,
+    this.myForm = this.fb.group({});
+    console.log(Object.assign(Inputs.inputs));
+
+    this.dynamicForm.forEach((formItem) => {
+      this.myForm.addControl(
+        formItem.controlName,
+        this.fb.control(
+          formItem.value,
+          formItem.required ? Validators.required : null
+        )
+      );
     });
-    this.dynamicForm = {
-      type: 'text',
-      label: 'This is a label',
-    };
   }
 }
--- src/app/shared/models/dynamic-form.model.ts
+++ src/app/shared/models/dynamic-form.model.ts
@@ -1,4 +1,11 @@
+import { ValidatorFn } from '@angular/forms';
+
 export interface DynamicForm {
+  controlName: string;
   type: 'text' | 'select';
   label: string;
+  selectMenuOptions?: { [key: string]: string };
+  value?: string | number;
+  // validators?: [ValidatorFn];
+  required: boolean;
 }
